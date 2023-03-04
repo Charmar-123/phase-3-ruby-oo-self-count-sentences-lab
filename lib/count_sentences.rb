ec5b4726fa15c0113f5f60ed2dd835ca4ebdd510
Charbel Maroun <charbel-maroun@hotmail.com>
2023-03-04 01:26:22 -0500

    commit tree ddc968c3dc5e146f8796ef939963a038974f2e1d

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
+    self.end_with?(".")
     
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    count = 0
+    ltr_array = self.split(/\.|\?|!/)
+    ltr_array.each do |ltr|
+      if ltr.size > 0
+        count += 1
+      end
+    end
+    count
   end
 end
